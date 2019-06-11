b5c6b3be20ede0e0fb75647a29f441d7a43296d7
rottencandy <md.saud020@gmail.com>
2019-06-11 19:38:14 +0530

    Fix login component and logo

--- webui-src/app/login.js
+++ webui-src/app/login.js
@@ -1,58 +1,55 @@
-'use strict';
 var m = require('mithril');
 var rs = require('rswebui');
 
 let onSuccessCallback = function() {};
 
 function renderLoginPage(callback) {
-    // Cannot use mount because vDOM will not let it override
-    m.render(document.body, loginComponent.view());
-    onSuccessCallback = callback;
+  // Cannot use mount because vDOM will not let any other mount overrides
+  m.render(document.body, m(loginComponent));
+  onSuccessCallback = callback;
 }
 
 let loginComponent = {
-    view: function() {
-        return m('.frame-center', [
-            m('img.logo[src=../../data/retroshare.svg][alt=retroshare_icon]'),
-            m('input.field[type=text][placeholder=Username][id=uname]'),
-            m('input.field[type=password][placeholder=Password][id=passwd]'),
-            m('button.submit-btn', {onclick: verifyLogin},
-              'Login'),
-            m('p.error[id=error]'),
-        ]);
-    }
+  view : function() {
+    return m('.frame-center', [
+      m('img.logo[src=../../data/retroshare.svg][alt=retroshare_icon][width=10%]'),
+      m('input.field[type=text][placeholder=Username][id=uname]'),
+      m('input.field[type=password][placeholder=Password][id=passwd]'),
+      m('button.submit-btn', {onclick : verifyLogin}, 'Login'),
+      m('p.error[id=error]'),
+    ]);
+  }
 };
 
 let uname = '';
 let passwd = '';
 
 function verifyLogin() {
-    [uname, passwd] = getKeys();
-    let loginHeader = {'Authorization': 'Basic ' + btoa(uname + ':' + passwd)};
-    rs.rsJsonApiRequest(
-        '/rsPeers/GetRetroshareInvite', {}, onResponse, true, loginHeader);
+  [uname, passwd] = getKeys();
+  let loginHeader = {'Authorization' : 'Basic ' + btoa(uname + ':' + passwd)};
+  rs.rsJsonApiRequest('/rsPeers/GetRetroshareInvite', {}, onResponse, true,
+                      loginHeader);
 }
 
 function getKeys() {
-    let uname = document.getElementById('uname').value;
-    let passwd = document.getElementById('passwd').value;
-    return [uname, passwd];
+  let uname = document.getElementById('uname').value;
+  let passwd = document.getElementById('passwd').value;
+  return [ uname, passwd ];
 }
 
 function onResponse(data, successful) {
-    if (successful) {
-        rs.setKeys(uname, passwd);
-        onSuccessCallback();
-    } else {
-        displayErrorMessage();
-    }
+  if (successful) {
+    rs.setKeys(uname, passwd);
+    onSuccessCallback();
+  } else {
+    displayErrorMessage();
+  }
 }
 
 function displayErrorMessage() {
-    m.render(document.getElementById('error'), 'Incorrent login/password.');
+  m.render(document.getElementById('error'), 'Incorrent login/password.');
 }
 
 module.exports = {
-    renderLoginPage,
+  renderLoginPage,
 }
-
