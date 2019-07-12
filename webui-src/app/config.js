ffc1a81f5a21eb427b8c9841041c5035f95a45b9
rottencandy <md.saud020@gmail.com>
2019-07-12 20:04:07 +0530

    Add minor theme improvements

--- webui-src/app/config.js
+++ webui-src/app/config.js
@@ -5,7 +5,8 @@ function sidebar(links) {
   return m('.sidebar',
     Object.keys(links)
     .map(function(panelName) {
-      return m('a.sidebar-link' + (Panel.active === panelName ? '#selected-sidebar-link' : ''), {
+      return m('a.sidebar-link' + (Panel.active === panelName ?
+          '#selected-sidebar-link' : ''), {
           onclick: function() {
             Panel.active = panelName;
           },
@@ -18,7 +19,6 @@ function sidebar(links) {
 class Panel {
   static component() {
     return [
-      sidebar(Panel.list),
       m(Panel.list[Panel.active]),
     ];
   }
@@ -60,15 +60,20 @@ new Panel('Network', {
   // TODO show info from UI hover message
   view: function() {
     return m('.node-panel', [
-      m('h3', 'Network Configuration'),
-      m('ul', [
-        m('li', ['Download limit(KB/s):', m('input#download-limit[type=number][name=download]', {
-          onblur: setMaxRates,
-        })]),
-        m('li', ['Upload limit(KB/s):', m('input#upload-limit[type=number][name=upload]', {
-          onblur: setMaxRates,
-        })]),
-      ])
+      m('.widget.widget-half', [
+        m('h3', 'Network Configuration'),
+        m('hr'),
+        m('ul', [
+          m('li', ['Download limit(KB/s):', m(
+            'input#download-limit[type=number][name=download]', {
+              onblur: setMaxRates,
+            })]),
+          m('li', ['Upload limit(KB/s):', m(
+            'input#upload-limit[type=number][name=upload]', {
+              onblur: setMaxRates,
+            })]),
+        ])
+      ]),
     ]);
   },
 });
@@ -86,14 +91,17 @@ new Panel('Node', {
   },
   view: function() {
     return m('.node-panel', [
-      m('h3', 'Public Information'),
-      m('ul', [
-        m('li', 'Name: ' + nodeInfo['ownName']),
-        m('li', 'Location ID: ' + nodeInfo['ownId']),
-        m('li', 'Firewall: ' + nodeInfo['firewalled']),
-        m('li', 'Port Forwarding: ' + nodeInfo['forwardPort']),
-        m('li', 'DHT: ' + nodeInfo['DHTActive']),
-        m('li', 'uPnP: ' + nodeInfo['uPnPActive']),
+      m('.widget.widget-half', [
+        m('h3', 'Public Information'),
+        m('hr'),
+        m('ul', [
+          m('li', 'Name: ' + nodeInfo['ownName']),
+          m('li', 'Location ID: ' + nodeInfo['ownId']),
+          m('li', 'Firewall: ' + nodeInfo['firewalled']),
+          m('li', 'Port Forwarding: ' + nodeInfo['forwardPort']),
+          m('li', 'DHT: ' + nodeInfo['DHTActive']),
+          m('li', 'uPnP: ' + nodeInfo['uPnPActive']),
+        ]),
       ]),
     ]);
   },
@@ -108,26 +116,31 @@ let servicesInfo = {
 
 new Panel('Services', {
   oninit: function() {
-    rs.rsJsonApiRequest('/rsServiceControl/getOwnServices', {}, servicesInfo.setData);
+    rs.rsJsonApiRequest('/rsServiceControl/getOwnServices', {},
+      servicesInfo.setData);
   },
   view: function() {
     return m('.node-panel', [
-      m('h3', 'My Services'),
-      m('table', [
-        m('tr', [
-          m('th', 'Name'),
-          m('th', 'ID'),
-          m('th', 'Version'),
+      m('.widget', [
+        m('h3', 'My Services'),
+        m('hr'),
+        m('table', [
+          m('tr', [
+            m('th', 'Name'),
+            m('th', 'ID'),
+            m('th', 'Version'),
+          ]),
+          servicesInfo.list.map(function(service) {
+            return m('tr', {
+              key: service.key,
+            }, [
+              m('td', service.value.mServiceName),
+              m('td', service.value.mServiceType),
+              m('td', service.value.mVersionMajor + '.' +
+                service.value.mVersionMinor),
+            ]);
+          }),
         ]),
-        servicesInfo.list.map(function(service) {
-          return m('tr', {
-            key: service.key,
-          }, [
-            m('td', service.value.mServiceName),
-            m('td', service.value.mServiceType),
-            m('td', service.value.mVersionMajor + '.' + service.value.mVersionMinor),
-          ]);
-        }),
       ]),
     ]);
   },
@@ -136,7 +149,8 @@ new Panel('Services', {
 
 let component = {
   view: function() {
-    return m('.tab', [
+    return m('.tab-page.fadein', [
+      sidebar(Panel.list),
       Panel.component(),
     ]);
   },
--- webui-src/app/home.js
+++ webui-src/app/home.js
@@ -4,38 +4,39 @@ var rs = require('rswebui');
 
 let CERT = '';
 let component = {
-    oninit: getCert,
-    view: function() {
-        return m('.tab-page.fadein', [
-            m('.widget.widget-half', [
-            m('h3', 'Certificate'),
-            m('p', 'Your Retroshare certificate, click to copy'),
-            m('hr'),
-            m('textarea.field[id=certificate][rows=14][cols=65][placeholder=certificate][readonly]', {
-                onclick: copyToClipboard,
-            },
-              CERT),
-            ]),
-        ]);
-    }
+  oninit: getCert,
+  view: function() {
+    return m('.tab-page.fadein', [
+      m('.widget.widget-half', [
+        m('h3', 'Certificate'),
+        m('p', 'Your Retroshare certificate, click to copy'),
+        m('hr'),
+        m('textarea.field[id=certificate][rows=14][cols=65][placeholder=certificate][readonly]', {
+            onclick: copyToClipboard,
+          },
+          CERT),
+      ]),
+    ]);
+  }
 };
 
 function getCert() {
-    let handleCert = function(body, state) {
-        if (state === true) {
-            CERT = body['retval'];
-        }
-    };
-    rs.rsJsonApiRequest('/rsPeers/GetRetroshareInvite', {}, handleCert);
+  let handleCert = function(body, state) {
+    if(state === true) {
+      CERT = body['retval'];
+    }
+  };
+  rs.rsJsonApiRequest('/rsPeers/GetRetroshareInvite', {}, handleCert);
 }
 
 function copyToClipboard() {
-    document.getElementById('certificate').select();
-    document.execCommand('copy');
+  document.getElementById('certificate')
+    .select();
+  document.execCommand('copy');
 };
 
 new rs.Tab('home', component);
 module.exports = {
-    component,
+  component,
 };
 
