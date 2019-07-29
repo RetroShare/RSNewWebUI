2997ed7a5c6c03b9e60e43dcf39e5e78d7465016
rottencandy <md.saud020@gmail.com>
2019-07-29 19:30:32 +0530

    display last connect, locations of friend nodes

--- webui-src/app/network.js
+++ webui-src/app/network.js
@@ -4,12 +4,31 @@ let rs = require('rswebui');
 
 const Node = () => {
   return {
+    isExpanded: false,
     view: (vnode) => m('.friend', {
       key: vnode.attrs.data[0].gpg_id
     }, [
-      m('.grid-2col', [
-        m('p', 'Name   :'), m('p', vnode.attrs.data[0].name),
-      ]),
+      m('i.fas.fa-angle-right', {
+        class: 'fa-rotate-' + (vnode.state.isExpanded ? '90' : '0'),
+        onclick: () => {
+          vnode.state.isExpanded = !vnode.state.isExpanded;
+          console.log(vnode.attrs.data)
+        },
+      }),
+      m('i.fas.fa-2x.fa-user-circle'),
+      m('span', vnode.attrs.data[0].name),
+      m('.details', {
+        style: "display:" + (vnode.state.isExpanded ? "block" : "none"),
+      }, [
+        m('.grid-2col', [
+          m('p', 'Last contact:'),
+          m('p', new Date(vnode.attrs.data[0].lastConnect * 1000).toDateString()),
+        ]),
+        m('h4', 'Locations'),
+        vnode.attrs.data.map((loc) => m('.location', [
+          m('i.fas.fa-user-tag'), m('span', loc.location),
+        ])),
+      ])
     ]),
   };
 };
@@ -21,7 +40,6 @@ const Friends = () => {
       rs.rsJsonApiRequest(
         '/rsPeers/getFriendList', {},
         (friendListIds) => {
-          console.log(friendListIds)
           friendListIds.sslIds.map(
             (sslId) => rs.rsJsonApiRequest(
               '/rsPeers/getPeerDetails', {
