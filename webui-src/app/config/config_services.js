9f6053ec3a68e4accd7ee41cd8e72463c1415982
rottencandy <md.saud020@gmail.com>
2019-08-09 19:00:02 +0530

    Add option to set default service status

--- webui-src/app/config/config_services.js
+++ webui-src/app/config/config_services.js
@@ -8,37 +8,73 @@ let servicesInfo = {
   },
 };
 
-let Services = {
+let Service = () => {
+  let defaultAllowed = undefined;
+  return {
+    oninit: (v) => rs.rsJsonApiRequest(
+      '/rsServiceControl/getServicePermissions', {
+        serviceId: v.attrs.data.key
+      }, (retval) => defaultAllowed = retval.permissions.mDefaultAllowed),
+    view: (v) => m('tr', {
+      key: v.attrs.data.key,
+    }, [
+      m('td', v.attrs.data.value.mServiceName),
+      m('td', v.attrs.data.value.mServiceType),
+      m('td', v.attrs.data.value.mVersionMajor +
+        '.' +
+        v.attrs.data.value.mVersionMinor),
+      m('td', m('input[type=checkbox]', {
+        checked: defaultAllowed,
+        oninput: (e) => {
+          defaultAllowed = e.target.checked;
+          rs.rsJsonApiRequest(
+            '/rsServiceControl/updateServicePermissions', {
+              serviceId: v.attrs.data.key,
+              permissions: {
+                mDefaultAllowed: defaultAllowed
+              }
+            });
+        },
+      })),
+    ]),
+  };
+};
+
+let MyServices = {
   oninit: function() {
     rs.rsJsonApiRequest('/rsServiceControl/getOwnServices', {},
       servicesInfo.setData);
   },
   view: function() {
-    return [
-      m('.widget', [
-        m('h3', 'My Services'),
-        m('hr'),
-        m('table', [
-          m('tr', [
-            m('th', 'Name'),
-            m('th', 'ID'),
-            m('th', 'Version'),
-          ]),
-          servicesInfo.list.map(function(service) {
-            return m('tr', {
-              key: service.key,
-            }, [
-              m('td', service.value.mServiceName),
-              m('td', service.value.mServiceType),
-              m('td', service.value.mVersionMajor +
-                '.' +
-                service.value.mVersionMinor),
-            ]);
-          }),
+    return m('.widget', [
+      m('h3', 'My Services'),
+      m('hr'),
+      m('table', [
+        m('tr', [
+          m('th', 'Name'),
+          m('th', 'ID'),
+          m('th', 'Version'),
+          m('th', 'Allow by default'),
         ]),
+        servicesInfo.list.map((data) => m(Service, {
+          data,
+        })),
       ]),
-    ];
+    ]);
   },
 };
 
-module.exports = Services;
+const Layout = () => {
+  return {
+    view: vnode => [
+      m(MyServices),
+    ],
+  };
+};
+
+module.exports = {
+  view: (vnode) => {
+    return m(Layout);
+  },
+};
+
