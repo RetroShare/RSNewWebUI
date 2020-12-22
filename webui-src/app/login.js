3c7324b09b172d936c43d09d36e4931897386901
Zener <zener@sbg.at>
2020-12-22 08:52:42 +0100

    simplify login with option to show username and url

--- webui-src/app/login.js
+++ webui-src/app/login.js
@@ -36,47 +36,68 @@ const verifyLogin = async function(uname, passwd, url) {
 
 function loginComponent() {
   var urlParams = new URLSearchParams(window.location.search);
-  let uname = urlParams.get('Username')||'';
+  let uname = urlParams.get('Username')||'webui';
   let passwd = '';
   let url = urlParams.get('Url') || window.location.protocol==='file:' ? 'http://127.0.0.1:9092' : window.location.protocol + '//' + window.location.host + window.location.pathname.replace('/index.html','');
+  let withOptions = false;
+  let logo = () => m('img.logo[width=30%]', {
+                         src: '../data/retroshare.svg',
+                         alt: 'retroshare_icon'
+                       });
+  let inputName = () => m('input', {
+                        id: 'username',
+                        type: 'text',
+                        value: uname,
+                        placeholder: 'Username',
+                        onchange: e => (uname = e.target.value)
+                      });
+  let inputPassword = () => m('input[autofocus]', {
+                        id: 'password',
+                        type: 'password',
+                        placeholder: 'Password',
+                        value: passwd,
+                        onchange: e => (passwd = e.target.value),
+                        onkeyup: e => {
+                            passwd = e.target.value;
+                            if (e.code==='Enter') loginBtn.click();
+                        }
+                      });
+  let inputUrl = () => m('input',{
+                       id: 'url',
+                       type: 'text',
+                       placeholder: 'Url',
+                       value: url,
+                       oninput: e => (url = e.target.value)
+                     });
+
+  let linkOptions = () => m('a',{
+                       onclick: e => withOptions=!withOptions,
+                     }, withOptions ? 'hide options' :'show options');
+
+  let buttonLogin = () => m('button.submit-btn', {
+                      id: 'loginBtn',
+                      onclick: () => verifyLogin(uname, passwd, url)
+                    }, 'Login');
+
+  let textError = () => m('p.error[id=error]');
   return {
     view: () => {
-      return m(
-        '.login-page',
-        m('.login-container', [
-          m('img.logo[width=30%]', {
-            src: '../data/retroshare.svg',
-            alt: 'retroshare_icon'
-          }),
-          m('input' + (uname === '' ? '[autofocus]':''), {
-            type: 'text',
-            value: uname,
-            placeholder: 'Username',
-            onchange: e => (uname = e.target.value)
-          }),
-          m('input'+ (uname !== '' ? '[autofocus]':''), {
-            type: 'password',
-            placeholder: 'Password',
-            onchange: e => (passwd = e.target.value),
-            onkeyup: e => {if (e.code==='Enter') loginBtn.click();}
-          }),
-          m('.extra',
-            m('input',{
-              type: 'text',
-              placeholder: 'Url',
-              value: url,
-              oninput: e => (url = e.target.value)
-            })
-          ),
-          m(
-            'button.submit-btn',
-            {
-              id: 'loginBtn',
-              onclick: () => verifyLogin(uname, passwd, url)
-            },
-            'Login'
-          ),
-          m('p.error[id=error]')
+      return m('.login-page',
+        m('.login-container', withOptions
+        ? [
+          logo(),
+          m(".extra",[m('label','Username:'),m('br'), inputName()]),
+          m(".extra",[m('label','Password:'),m('br'), inputPassword()]),
+          m(".extra",[m('label','Url:'),m('br'), inputUrl()]),
+          linkOptions(),
+          buttonLogin(),
+          textError()
+        ] : [
+          logo(),
+          inputPassword(),
+          linkOptions(),
+          buttonLogin(),
+          textError()
         ])
       );
     }
