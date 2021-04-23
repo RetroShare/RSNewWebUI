69b8e5acb2be795ce77e3444c26ff27471296806
Avinash Kumar <bhaiavinash9@gmail.com>
2021-04-23 17:55:49 +0530

    update changes

--- webui-src/app/people/people.js
+++ webui-src/app/people/people.js
@@ -3,68 +3,6 @@ const rs = require('rswebui');
 
 const peopleUtil = require('people/people_util');
 
-const contactInfo = () => {
-  let details = {};
-
-  return {
-    oninit: (v) =>
-      rs.rsJsonApiRequest(
-        '/rsIdentity/getIdDetails',
-        {
-          id: v.attrs.id.mGroupId,
-        },
-        (data) => {
-          details = data.details;
-        }
-      ),
-    view: (v) =>
-      m(
-        '.identity',
-        {
-          key: details.mId,
-          style: 'display:' + (v.attrs.id.isSearched ? 'block' : 'none'),
-        },
-        [
-          m('h4', details.mNickname),
-          m(peopleUtil.UserAvatar, { avatar: details.mAvatar }),
-          m('.details', [
-            m('p', 'ID:'),
-            m('p', details.mId),
-            m('p', 'Type:'),
-            m('p', details.mFlags === 14 ? 'Signed ID' : 'Anonymous ID'),
-            m('p', 'Owner node ID:'),
-            m('p', details.mPgpId),
-            m('p', 'Created on:'),
-            m(
-              'p',
-              typeof details.mPublishTS === 'object'
-                ? new Date(details.mPublishTS.xint64 * 1000).toLocaleString()
-                : 'undefiend'
-            ),
-            m('p', 'Last used:'),
-            m(
-              'p',
-              typeof details.mLastUsageTS === 'object'
-                ? new Date(details.mLastUsageTS.xint64 * 1000).toLocaleDateString()
-                : 'undefiend'
-            ),
-          ]),
-          m(
-            'button',
-            {
-              onclick: () =>
-                m.route.set('/chat/:userid/createdistantchat', {
-                  userid: v.attrs.id.mGroupId,
-                }),
-            },
-            'Chat'
-          ),
-          m('button.red', {}, 'Mail'),
-        ]
-      ),
-  };
-};
-
 const AllContacts = () => {
   const list = peopleUtil.sortUsers(rs.userList.users);
   return {
@@ -72,37 +10,14 @@ const AllContacts = () => {
       return m('.widget', [
         m('h3', 'Contacts', m('span.counter', list.length)),
         m('hr'),
-        list.map((id) => m(contactInfo, { id })),
+        list.map((id) => m(peopleUtil.regularcontactInfo, { id })),
       ]);
     },
   };
 };
-const SearchBar = () => {
-  let searchString = '';
-
-  return {
-    view: () =>
-      m('input.searchbar', {
-        type: 'text',
-        placeholder: 'search',
-        value: searchString,
-        oninput: (e) => {
-          searchString = e.target.value.toLowerCase();
-
-          rs.userList.users.map((id) => {
-            if (id.mGroupName.toLowerCase().indexOf(searchString) > -1) {
-              id.isSearched = true;
-            } else {
-              id.isSearched = false;
-            }
-          });
-        },
-      }),
-  };
-};
 
 const Layout = {
-  view: (vnode) => m('.tab-page', [m(SearchBar), m(AllContacts)]),
+  view: (vnode) => m('.tab-page', [m(peopleUtil.SearchBar), m(AllContacts)]),
 };
 
 module.exports = {
--- webui-src/app/people/people_util.js
+++ webui-src/app/people/people_util.js
@@ -1,5 +1,6 @@
 const rs = require('rswebui');
 const m = require('mithril');
+
 function checksudo(id) {
   return id === '0000000000000000';
 }
@@ -65,6 +66,91 @@ function ownIds(consumer = (list) => {}, onlySigned = false) {
     }
   });
 }
+const SearchBar = () => {
+  let searchString = '';
+
+  return {
+    view: () =>
+      m('input.searchbar', {
+        type: 'text',
+        placeholder: 'search',
+        value: searchString,
+        oninput: (e) => {
+          searchString = e.target.value.toLowerCase();
+
+          rs.userList.users.map((id) => {
+            if (id.mGroupName.toLowerCase().indexOf(searchString) > -1) {
+              id.isSearched = true;
+            } else {
+              id.isSearched = false;
+            }
+          });
+        },
+      }),
+  };
+};
+
+const regularcontactInfo = () => {
+  let details = {};
+
+  return {
+    oninit: (v) =>
+      rs.rsJsonApiRequest(
+        '/rsIdentity/getIdDetails',
+        {
+          id: v.attrs.id.mGroupId,
+        },
+        (data) => {
+          details = data.details;
+        }
+      ),
+    view: (v) =>
+      m(
+        '.identity',
+        {
+          key: details.mId,
+          style: 'display:' + (v.attrs.id.isSearched ? 'block' : 'none'),
+        },
+        [
+          m('h4', details.mNickname),
+          m(UserAvatar, { avatar: details.mAvatar }),
+          m('.details', [
+            m('p', 'ID:'),
+            m('p', details.mId),
+            m('p', 'Type:'),
+            m('p', details.mFlags === 14 ? 'Signed ID' : 'Anonymous ID'),
+            m('p', 'Owner node ID:'),
+            m('p', details.mPgpId),
+            m('p', 'Created on:'),
+            m(
+              'p',
+              typeof details.mPublishTS === 'object'
+                ? new Date(details.mPublishTS.xint64 * 1000).toLocaleString()
+                : 'undefiend'
+            ),
+            m('p', 'Last used:'),
+            m(
+              'p',
+              typeof details.mLastUsageTS === 'object'
+                ? new Date(details.mLastUsageTS.xint64 * 1000).toLocaleDateString()
+                : 'undefiend'
+            ),
+          ]),
+          m(
+            'button',
+            {
+              onclick: () =>
+                m.route.set('/chat/:userid/createdistantchat', {
+                  userid: v.attrs.id.mGroupId,
+                }),
+            },
+            'Chat'
+          ),
+          m('button.red', {}, 'Mail'),
+        ]
+      ),
+  };
+};
 
 module.exports = {
   sortUsers,
@@ -73,4 +159,6 @@ module.exports = {
   checksudo,
   UserAvatar,
   contactlist,
+  SearchBar,
+  regularcontactInfo,
 };
