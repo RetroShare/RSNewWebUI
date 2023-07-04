b029ca7bfea3c68f5fec5e827c54989d68976958
zelfroster <sumitkumarsoni123@gmail.com>
2023-07-04 12:55:50 +0530

    feat: compose mail working with a nice ui

--- webui-src/app/mail/mail_compose.js
+++ webui-src/app/mail/mail_compose.js
@@ -3,33 +3,56 @@ const rs = require('rswebui');
 const widget = require('widgets');
 
 const Layout = () => {
-  let subject;
-  let mailBody;
-  let identity;
-  let inputTo;
-  let inputList;
-  const recipients = {
-    to: [],
-    cc: [],
-    bcc: [],
+  const data = {
+    subject: '',
+    mailBody: '',
+    identity: null,
+    recipients: {
+      to: {
+        inputVal: '',
+        inputList: [],
+        sendList: [],
+      },
+      cc: {
+        inputVal: '',
+        inputList: [],
+        sendList: [],
+      },
+      bcc: {
+        inputVal: '',
+        inputList: [],
+        sendList: [],
+      },
+    },
   };
   return {
-    oninit: (v) => {
-      identity = v.attrs.ownId[0];
-      inputList = v.attrs.allUsers;
+    oninit: ({ attrs: { ownId, allUsers } }) => {
+      data.identity = ownId[0];
+      Object.keys(data.recipients).forEach((item) => {
+        data.recipients[item].inputList = allUsers;
+      });
     },
-    view: (v) => {
-      function handleInput(e) {
-        inputTo = e.target.value;
-        inputList = v.attrs.allUsers.filter((item) =>
+    view: ({ attrs: { ownId, allUsers } }) => {
+      // get recipientType from the function call to handle events for all recipient types
+      function handleInput(e, recipientType) {
+        data.recipients[recipientType].inputVal = e.target.value;
+        data.recipients[recipientType].inputList = allUsers.filter((item) =>
           item.mGroupName.toLowerCase().includes(e.target.value.toLowerCase())
         );
+        m.redraw();
       }
-      function handleClick(item) {
-        recipients.to.push(item);
+      function handleClick(item, recipientType) {
+        data.recipients[recipientType].sendList.push(item);
         // reset values
-        inputTo = '';
-        inputList = v.attrs.allUsers;
+        data.recipients[recipientType].inputVal = '';
+        data.recipients[recipientType].inputList = allUsers;
+        m.redraw();
+      }
+      function removeSelectedItem(recipient, recipientType) {
+        data.recipients[recipientType].sendList = data.recipients[recipientType].sendList.filter(
+          (item) => item.mGroupId !== recipient.mGroupId
+        );
+        m.redraw();
       }
       return m('.widget', [
         m('.widget__heading', m('h3', 'Compose a mail')),
@@ -39,69 +62,72 @@ const Layout = () => {
             m(
               'select[id=idtags]',
               {
-                value: identity,
-                onchange: (e) => (identity = v.attrs.ownId[e.target.selectedIndex]),
+                value: data.identity,
+                onchange: (e) => (data.identity = ownId[e.target.selectedIndex]),
               },
-              v.attrs.ownId &&
-                v.attrs.ownId.map((id) =>
+              ownId &&
+                ownId.map((id) =>
                   m(
                     'option',
                     { value: id },
                     rs.userList.userMap[id]
-                      ? rs.userList.userMap[id].toLocaleString() + ' (' + id.slice(0, 8) + '...)'
+                      ? rs.userList.userMap[id].toLocaleString() + ' (' + id.slice(0, 12) + '...)'
                       : 'No Signature'
                   )
                 )
             ),
           ]),
           m('.compose-mail__recipients', [
-            m('label.bold', 'To: '),
-            m('.recipients__to', [
-              recipients.to &&
-                recipients.to.length > 0 &&
-                recipients.to.map((recipient) =>
-                  m('.recipients__to__selected', [
-                    m('span', recipient.mGroupName),
-                    m('i.fas.fa-times', {
-                      onclick: () => {
-                        recipients.to = recipients.to.filter(
-                          (item) => item.mGroupId !== recipient.mGroupId
-                        );
-                        m.redraw();
-                      },
+            Object.keys(data.recipients).map((recipientType) =>
+              m('.compose-mail__recipients__container', [
+                m('label.bold', `${recipientType}: `),
+                m('.recipients', [
+                  data.recipients[recipientType].sendList.length > 0 &&
+                    data.recipients[recipientType].sendList.map((recipient) =>
+                      m('.recipients__selected', [
+                        m('span', recipient.mGroupName),
+                        m('i.fas.fa-times', {
+                          onclick: () => removeSelectedItem(recipient, recipientType),
+                        }),
+                      ])
+                    ),
+                  m('.recipients__input', [
+                    m('input[type=text].recipients__input-field', {
+                      value: data.recipients[recipientType].inputVal,
+                      oninput: (e) => handleInput(e, recipientType),
                     }),
-                  ])
-                ),
-              m('.recipients__to__input', [
-                m('input[type=text].recipients__to__input-field', {
-                  value: inputTo,
-                  oninput: handleInput,
-                }),
-                m('ul.recipients__to__input-list[autocomplete=off]', [
-                  inputList && inputList.length > 0
-                    ? inputList.map((item) =>
-                        m('li', { onclick: () => handleClick(item) }, item.mGroupName)
-                      )
-                    : m('li', 'No Item'),
+                    m('ul.recipients__input-list[autocomplete=off]', [
+                      data.recipients[recipientType].inputList.length > 0
+                        ? data.recipients[recipientType].inputList.map((item) =>
+                            m(
+                              'li',
+                              { onclick: () => handleClick(item, recipientType) },
+                              item.mGroupName
+                            )
+                          )
+                        : m('li', 'No Item'),
+                    ]),
+                  ]),
                 ]),
-              ]),
-            ]),
+              ])
+            ),
           ]),
           m('input.compose-mail__subject[type=text][placeholder=Subject]', {
-            oninput: (e) => (subject = e.target.value),
+            oninput: (e) => (data.subject = e.target.value),
           }),
           m('textarea.compose-mail__message[placeholder=Message]', {
-            oninput: (e) => (mailBody = e.target.value),
-            value: mailBody,
+            oninput: (e) => (data.mailBody = e.target.value),
+            value: data.mailBody,
           }),
-          v.attrs.allUsers &&
+          allUsers &&
             m(
               'button.compose-mail__send-btn',
               {
                 onclick: () => {
-                  const to = recipients.to.map((toItem) => toItem.mGroupId);
-                  const cc = recipients.cc.map((toItem) => toItem.mGroupId);
-                  const bcc = recipients.bcc.map((toItem) => toItem.mGroupId);
+                  const to = data.recipients.to.sendList.map((toItem) => toItem.mGroupId);
+                  const cc = data.recipients.cc.sendList.map((ccItem) => ccItem.mGroupId);
+                  const bcc = data.recipients.bcc.sendList.map((bccItem) => bccItem.mGroupId);
+                  const { identity, subject, mailBody } = data;
                   rs.rsJsonApiRequest('/rsMsgs/sendMail', {
                     from: identity,
                     subject,
@@ -111,14 +137,14 @@ const Layout = () => {
                     bcc,
                   }).then((res) => {
                     if (res.body.retval) {
-                      recipients.to = [];
-                      recipients.cc = [];
-                      recipients.bcc = [];
-                      subject = '';
-                      mailBody = '';
+                      Object.keys(data.recipients).forEach((item) => {
+                        data.recipients[item].sendList = [];
+                      });
+                      data.subject = '';
+                      data.mailBody = '';
                       m.redraw();
                     }
-                    res.body.retval !== 1
+                    res.body.retval < 1
                       ? widget.popupMessage([m('h3', 'Error'), m('hr'), m('p', res.body.errorMsg)])
                       : widget.popupMessage([
                           m('h3', 'Success'),
