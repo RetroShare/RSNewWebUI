1c8aa0259f45b088f7d1dd4945ac5811e4b84a63
zelfroster <sumitkumarsoni123@gmail.com>
2023-08-28 02:04:29 +0530

    fix: fixed webui crash when file search has no matching results and fixed popup message when downloading from search results

--- webui-src/app/files/files_search.js
+++ webui-src/app/files/files_search.js
@@ -1,8 +1,8 @@
 const m = require('mithril');
 const rs = require('rswebui');
+const widget = require('widgets');
 const futil = require('files/files_util');
 const fproxy = require('files/files_proxy');
-const widget = require('widgets');
 
 let matchString = '';
 let currentItem = 0;
@@ -21,24 +21,42 @@ function handleSubmit() {
 const SearchBar = () => {
   return {
     view: () =>
-      m(
-        'form.search-form',
-        {
-          onsubmit: handleSubmit,
-        },
-        [
-          m('input[type=text][placeholder=search keyword]', {
-            value: matchString,
-            oninput: (e) => (matchString = e.target.value),
-          }),
-          m('button[type=submit]', m('i.fas.fa-search')),
-        ]
-      ),
+      m('form.search-form', { onsubmit: handleSubmit }, [
+        m('input[type=text][placeholder=search keyword]', {
+          value: matchString,
+          oninput: (e) => (matchString = e.target.value),
+        }),
+        m('button[type=submit]', m('i.fas.fa-search')),
+      ]),
   };
 };
 
 const Layout = () => {
   let active = 0;
+  function handleFileDownload(item) {
+    rs.rsJsonApiRequest('/rsFiles/FileRequest', {
+      fileName: item.fName,
+      hash: item.fHash,
+      flags: futil.RS_FILE_REQ_ANONYMOUS_ROUTING,
+      size: {
+        xstr64: item.fSize.xstr64,
+      },
+    })
+      .then((res) => {
+        widget.popupMessage(
+          m('.widget', [
+            m('.widget__heading', m('h3', m('i.fas.fa-file-medical'), ' File Download')),
+            m(
+              '.widget__body',
+              m('p', `File is ${res.body.retval ? 'getting' : 'already'} downloaded.`)
+            ),
+          ])
+        );
+      })
+      .catch((error) => {
+        console.log('error in sending download request: ', error);
+      });
+  }
   return {
     view: () => [
       m('.widget__heading', [m('h3', 'Search'), m(SearchBar)]),
@@ -46,25 +64,26 @@ const Layout = () => {
         m('div.file-search-container', [
           m('div.file-search-container__keywords', [
             m('h5.bold', 'Keywords'),
-            m(
-              'div.keywords-container',
-              Object.keys(reqObj)
-                .reverse()
-                .map((item, index) => {
-                  return m(
-                    m.route.Link,
-                    {
-                      class: active === index ? 'selected' : '',
-                      onclick: () => {
-                        active = index;
-                        currentItem = item;
+            Object.keys(reqObj).length !== 0 &&
+              m(
+                'div.keywords-container',
+                Object.keys(reqObj)
+                  .reverse()
+                  .map((item, index) => {
+                    return m(
+                      m.route.Link,
+                      {
+                        class: active === index ? 'selected' : '',
+                        onclick: () => {
+                          active = index;
+                          currentItem = item;
+                        },
+                        href: `/files/search/${item}`,
                       },
-                      href: '/files/search/' + item,
-                    },
-                    reqObj[item]
-                  );
-                })
-            ),
+                      reqObj[item]
+                    );
+                  })
+              ),
           ]),
           m('div.file-search-container__results', [
             Object.keys(fproxy.fileProxyObj).length === 0
@@ -81,49 +100,19 @@ const Layout = () => {
                   ),
                   m(
                     'tbody.results',
-                    fproxy.fileProxyObj[currentItem.slice(1)] === undefined &&
-                      fproxy.fileProxyObj[currentItem.slice(1)].length === 0
-                      ? 'Fetching Results...'
-                      : fproxy.fileProxyObj[currentItem.slice(1)].map((item) =>
+                    fproxy.fileProxyObj[currentItem.slice(1)]
+                      ? fproxy.fileProxyObj[currentItem.slice(1)].map((item) =>
                           m('tr', [
                             m('td.results__name', [m('i.fas.fa-file'), m('span', item.fName)]),
                             m('td.results__size', rs.formatBytes(item.fSize.xint64)),
                             m('td.results__hash', item.fHash),
                             m(
                               'td.results__download',
-                              m(
-                                'button',
-                                {
-                                  onclick: () => {
-                                    rs.rsJsonApiRequest('/rsFiles/FileRequest', {
-                                      fileName: item.fName,
-                                      hash: item.fHash,
-                                      flags: futil.RS_FILE_REQ_ANONYMOUS_ROUTING,
-                                      size: {
-                                        xstr64: item.fSize.xstr64,
-                                      },
-                                    })
-                                      .then((res) => {
-                                        res.retval
-                                          ? widget.popupMessage([
-                                              m('i.fas.fa-file-medical'),
-                                              m('h3', 'File is being downloaded!'),
-                                            ])
-                                          : widget.popupMessage([
-                                              m('i.fas.fa-file-medical'),
-                                              m('h3', 'File is already downloaded!'),
-                                            ]);
-                                      })
-                                      .catch((error) => {
-                                        console.log('error in sending download request: ', error);
-                                      });
-                                  },
-                                },
-                                'Download'
-                              )
+                              m('button', { onclick: () => handleFileDownload(item) }, 'Download')
                             ),
                           ])
                         )
+                      : 'No Results.'
                   ),
                 ]),
           ]),
@@ -134,7 +123,5 @@ const Layout = () => {
 };
 
 module.exports = {
-  view: (vnode) => {
-    return m(Layout);
-  },
+  view: () => m(Layout),
 };
